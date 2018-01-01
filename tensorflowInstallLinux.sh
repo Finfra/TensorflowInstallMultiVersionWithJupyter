53f55e5e3f5f9e3363d607909970ba21af0b9ff3
NamJungGu <jgnam73@hotmail.com>
2018-01-01 10:18:25 +0900

    pip -> pip2

--- tensorflowInstallLinux.sh
+++ tensorflowInstallLinux.sh
@@ -18,9 +18,9 @@ fi
 
 
 # Check Virtualenvs and install
-x=`pip list --format=columns|grep virtualenvwrapper|wc -l`
+x=`pip2 list --format=columns|grep virtualenvwrapper|wc -l`
 if [ $x = "0" ]; then
-    sudo pip install virtualenv virtualenvwrapper
+    sudo pip2 install virtualenv virtualenvwrapper
 fi
 
 # Check Virtualenvs env and setting
@@ -36,81 +36,81 @@ fi
 
 ###### Tensorflow Install ##########################
 mkvirtualenv -p python2.7 tf0.11cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc2-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf0.11cp27
 
 mkvirtualenv -p python3.5 tf0.11cp35
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.11.0rc2-cp35-cp35m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf0.11p35
 
 mkvirtualenv -p python3.6 tf0.12cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf0.12cp27
 
 mkvirtualenv -p python3.6 tf0.12cp35
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf0.12p35
 
 mkvirtualenv -p python2.7 tf1.0cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.0.0rc2-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.0cp27
 
 mkvirtualenv -p python3.6 tf1.0cp36
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.0.0rc2-cp36-cp36m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.0cp36
 
 mkvirtualenv -p python2.7 tf1.1cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.1.0rc2-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.1cp27
 
 mkvirtualenv -p python3.6 tf1.1p36
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.1.0rc2-cp36-cp36m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.1p3.6
 
 mkvirtualenv -p python2.7 tf1.2cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.2.1-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.2cp27
 
 mkvirtualenv -p python3.6 tf1.2p36
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.2.1-cp36-cp36m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.2p3.6
 
 mkvirtualenv -p python2.7 tf1.3cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.3.0rc2-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.3cp27
 
 mkvirtualenv -p python3.6 tf1.3p36
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.3.0rc2-cp36-cp36m-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.3p3.6
 
 mkvirtualenv -p python2.7 tf1.4cp27
-pip install ipython ipykernel seaborn
+pip2 install ipython ipykernel seaborn
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.4.0rc1-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+pip2 install --upgrade $TF_BINARY_URL
 python -m ipykernel install --user --name=tf1.4cp27
 
 ###### Check #######################################
